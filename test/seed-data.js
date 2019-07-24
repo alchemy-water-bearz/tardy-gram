ad325c6ac37b1e7d239f1a707a8f0aa230728cec
Lili Boxer <ninaliboxer@gmail.com>
2019-07-24 14:30:10 -0700

    passing

--- test/seed-data.js
+++ test/seed-data.js
@@ -3,7 +3,7 @@ const Post = require('../lib/models/Post');
 const Comment = require('../lib/models/Comment');
 const chance = require('chance').Chance();
 
-module.exports = async({ users = 1, posts = 20, comments = 5 } = { users: 5, posts: 5, comments: 5 }) => {
+module.exports = async({ users = 1, posts = 20, comments = 5 } = { users: 5, posts: 50, comments: 100 }) => {
   const createdUsers = await User.create(
     [...Array(users)].map(() => ({
       username: chance.animal({ type: 'grassland' }),
@@ -12,15 +12,22 @@ module.exports = async({ users = 1, posts = 20, comments = 5 } = { users: 5, pos
     }))
   );
 
+  const userPost = await Post.create({
+    user: createdUsers[0]._id,
+    photoURL: chance.url({ extensions: ['gif', 'jpg', 'png'] }),
+    caption: chance.sentence(),
+    tags: [chance.hashtag()]
+  });
+
   const createdPost = await Post.create(
-    [...Array(posts)].map(() => ({
+    [...Array(posts - 1)].map(() => ({
       user: chance.pickone(createdUsers)._id,
       photoURL: chance.url({ extensions: ['gif', 'jpg', 'png'] }),
       caption: chance.sentence(),
       tags: [chance.hashtag()]
     }))
   );
-  
+
   const createdComment = await Comment.create(
     [...Array(comments)].map(() => ({
       commentBy: chance.pickone(createdUsers)._id,
@@ -29,9 +36,15 @@ module.exports = async({ users = 1, posts = 20, comments = 5 } = { users: 5, pos
     }))
   );
 
+  const userComment = await Comment.create({
+    commentBy: createdUsers[0]._id,
+    post: chance.pickone(createdPost)._id,
+    comment: chance.paragraph({ setences: 2 })
+  });
+
   return {
     users: createdUsers,
-    posts: createdPost,
-    comments: createdComment
+    posts: [...createdPost, userPost],
+    comments: [...createdComment, userComment]
   };
 };
