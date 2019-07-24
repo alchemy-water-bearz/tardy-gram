113a4299ac8735b9b1ecc3aec12057c0cc834b77
Lili Boxer <ninaliboxer@gmail.com>
2019-07-24 09:49:55 -0700

    passed post create route

--- test/post.test.js
+++ test/post.test.js
@@ -1,39 +1,12 @@
-require('dotenv').config();
+const { getAgent, getUsers, getPosts, getComments } = require('./data-helpers');
 
-const request = require('supertest');
-const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
-
-const User = require('../lib/models/User');
-const Post = require('../lib/models/Post');
-const Comment = require('../lib/models/Comment');
 
 describe('Post Routes', () => {
-  beforeAll(() => {
-    connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
 
   it('can create a post', async() => {
-    const user = JSON.parse(JSON.stringify(
-      await User.create({ 
-        username: 'Danny',
-        profilePhoto: 'some url',
-        password: 'password'
-      })
-    ));
-    return request(app)
+    return getAgent()
       .post('/api/v1/posts')
       .send({ 
-        user, 
         photoURL: 'another url',
         caption: 'Danny and Claire are awesome',
         tags: ['mobbing', 'alchemy']
@@ -42,7 +15,7 @@ describe('Post Routes', () => {
         expect(res.body).toEqual({
           _id: expect.any(String), 
           __v: 0,
-          user: user._id.toString(),
+          user: expect.any(String),
           photoURL: 'another url',
           caption: 'Danny and Claire are awesome',
           tags: ['mobbing', 'alchemy']
